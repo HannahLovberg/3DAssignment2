9d825f0a90b45fc6ba2a1d5304bd9427c5a10655
Hannah <Hannah@HANNAHDATOR>
2016-12-10 19:18:21 +0100

    push

--- Fragment.glsl
+++ Fragment.glsl
@@ -1,12 +1,11 @@
 #version 400
 in vec3 color;
-in vec2 TexCoord;
 
 out vec4 fragment_color;
 
-uniform sampler2D ourTexture; 
+
 
 void main () 
 {
-	fragment_color = texture(ourTexture, TexCoord) * vec4(color, 1.0f);
+	fragment_color = vec4(0.0f, 1.0f, 0.0f, 1.0f);
 }
--- GeometryShader.glsl
+++ GeometryShader.glsl
@@ -1,21 +1,11 @@
-#version 400
-layout (triangle) in;
-layout (triangle_strip, max_vertices = 4) out;
+#version 330 core
+layout (points) in;
+layout (points, max_vertices = 1) out;
 
 void main()
 {
 	gl_Position = gl_in[0].gl_Position;
 	EmitVertex();	
-												   
-	gl_Position = gl_in[1].gl_Position;
-	EmitVertex();		
-					
-	gl_Position = gl_in[2].gl_Position;
-	EmitVertex();		
-					
-	gl_Position = gl_in[0].gl_Position;
-	EmitVertex();
-
 	EndPrimitive();
 
 }
