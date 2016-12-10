d340ef2842fc8a922a4de0bfe5ec08b236c2056f
Hannah <Hannah@HANNAHDATOR>
2016-12-10 22:48:27 +0100

    push

--- VertexShader.glsl
+++ VertexShader.glsl
@@ -1,10 +1,14 @@
+#version 430
+in vec3 vertex_position;
+
 uniform mat4 viewMatrix;
 uniform mat4 projectionMatrix;
 uniform mat4 modelMatrix;
 
 void main()
 {	
-  vec4 world_pos = modelMatrix * gl_Vertex;
+  vec4 world_pos = modelMatrix * vertex_position;
   vec4 view_pos = viewMatrix * world_pos;
   gl_Position = projectionMatrix * view_pos;
 }
+
